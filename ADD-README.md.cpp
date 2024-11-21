77345deca2e74fc35e84792d8d5202c7133c3ebf
tasbiha60 <htjtasbi@gmail.com>
2024-11-21 16:23:20 +0600

    Create ADD-README.md.cpp
    Physics2-Assignment#3
    CSE60-SectionA
    Metropolitan University, Bangladesh
    Hussain Tasbiha Jannat(241-115-039)
    Tasnia sultana(241-115-009)
    Mst Rabina(241-115-029)
    Sumaiya Ahsan Tamanna(241-115-026)

---
+++ ADD-README.md.cpp
@@ -0,0 +1,7 @@
+Physics2-Assignment#3
+CSE60-SectionA
+Metropolitan University, Bangladesh
+Hussain Tasbiha Jannat(241-115-039)
+Tasnia sultana(241-115-009)
+Mst Rabina(241-115-029)
+Sumaiya Ahsan Tamanna(241-115-026)
